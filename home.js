10c0395496de14772351d94966118b6dceaa8ca3
Noahoneal001 <143848565+Noahoneal001@users.noreply.github.com>
2024-05-13 15:46:35 -0700

    SAVING....

--- home.js
+++ home.js
@@ -1,2 +1,31 @@
-/* ihjd
- uhds*/
+let dropdownDeployed = false
+
+document.addEventListener(`DOMContentLoaded`, () => {
+
+
+
+    addListener(`portfolioButtonHeader`, `click`, (eventData) => {
+
+        if(!dropdownDeployed) {
+            document.getElementById(`dropdown`).style.height = `200px`
+            dropdownDeployed = true
+        }
+
+    })
+
+    document.addEventListener(`click`, (eventData) => {
+        if(!eventData.target.matches(`.dropdown`)) {
+            document.getElementById(`dropdown`).style.height = `0px`
+            dropdownDeployed = false
+        }
+    })
+
+})
+
+function getById(id) {
+    return document.getElementById(id)
+}
+
+function addListener(id, event, code) {
+    getById(id).addEventListener(event, (eventData) => {code(eventData)})
+}
